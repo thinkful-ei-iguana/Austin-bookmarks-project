14bfc4715bf60c04e994e23fd2307a3f32e5ecf4
AustinT <austintumlinson@gmail.com>
2019-11-07 08:31:00 -0600

    built more api functionality

--- api.js
+++ api.js
@@ -2,6 +2,28 @@
 //import cuid from './cuid'; ??
 const BASE_URL = ' https://thinkful-list-api.herokuapp.com/austin';
 
+const listApiFetch = function (...args) {
+    let error;
+    return fetch(...args)
+    .then(res => {
+        if (!res.ok) {
+            error = { code: res.status};
+        if (!res.headers.get('content-type').includes('json')) {
+            error.message = res.statusText;
+            return Promise.reject(error);
+            }
+        }
+        return res.json();
+    })
+    .then(data => {
+        if (error) {
+            error.message = data.message;
+            return Promise.reject(error);
+        }
+        return data;
+    });
+};
+
 const validateName = function(title) {
     if (!title) {
         throw new TypeError('Must Provide Name');
@@ -14,19 +36,45 @@ const validateUrl = function(url) {
     }
 };
 
-const createBookmark = function (title, rating, url, description) {
-    return {
-        //id: cuid(), ???
-        title,
-        rating,
-        url,
-        description,
-        expanded: false
-    };
+const getBookmarks = function () {
+    return listApiFetch(`${BASE_URL}/bookmarks`);
 };
+
+const newBookmark = function (id, title, rating, url, description) {
+    const newInput = JSON.stringify({ id, title, rating, url, description });
+    return listApiFetch(`${BASE_URL/bookmarks}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: newInput
+    });
+};
+
+// const createBookmark = function (title, rating, url, description) {
+//     return {
+//         //id: cuid(), ???
+//         title,
+//         rating,
+//         url,
+//         description,
+//         expanded: false
+//     };
+// };
+
+
+const deleteBookmark = function (id) {
+    return listApiFetch(BASE_URL + '/bookmarks/' + id, {
+        method: 'DELETE'
+    });
+};
+
+
 
 export default {
     validateName,
     validateUrl,
-    createBookmark
+    newBookmark,
+    getBookmarks,
+    deleteBookmark
 };
